e778a5eb1f7ddefc07a18376f86209d97eb85830
Ryan <ryanjennett@gmail.com>
2025-11-19 22:48:38 -0700

    Add todo comments to main.cpp, begin scaffold of test_rrt.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -7,8 +7,16 @@ using namespace std;
 
 int main()
 {
+    // Test build
     int var;
     cout << "Hello potato, how many penguins?" << endl;
     cin >> var;
     cout << "Penguins to feed: " << var << endl;
+
+    // Initialize general configuration space as array of coordinates
+    // Initialize start coordinates
+    // Initialize goal coordinates
+    // Initialize obstacle coordinates
+    // Initialize obstacle dimensions
+    // (LATER) Differentiate the set of free coordinates from obs coordinates(?)
 }
---
+++ tests/test_rrt.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+#define private public
+#include "../src/Edge.h"
+#include "../src/Graph.h"
+#include "../src/Node.h"
+
+using namespace std;
+
+class test_rrt : public ::testing::Test
+{
+};
